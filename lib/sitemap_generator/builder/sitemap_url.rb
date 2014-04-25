d7df35beaf93ffdffb865f63c99bc0b8cedeeb95
Ryan Siddle <ryan@whitestarmedia.co.uk>
2014-04-25 10:54:18 +0100

    Remove all media

--- lib/sitemap_generator/builder/sitemap_url.rb
+++ lib/sitemap_generator/builder/sitemap_url.rb
@@ -134,8 +134,7 @@ module SitemapGenerator
 
           self[:alternates].each do |alternate|
             rel = alternate[:nofollow] ? 'alternate nofollow' : 'alternate'
-            media = !alternate[:media].blank? ? alternate[:media] : nil
-            builder.xhtml :link, :rel => rel, :hreflang => alternate[:lang].to_s, :href => alternate[:href].to_s, :media => media
+            builder.xhtml :link, :rel => rel, :hreflang => alternate[:lang].to_s, :href => alternate[:href].to_s
           end
 
           unless SitemapGenerator::Utilities.blank?(self[:geo])
